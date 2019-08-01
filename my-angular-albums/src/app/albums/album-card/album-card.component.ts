33d0474c63429fd3b5ccdf76a014d7cca16c52ad
Kevin Owusu-Sekyere <kowusu1@umbc.edu>
2019-08-01 08:53:46 -0400

    thurs morn

--- my-angular-albums/src/app/albums/album-card/album-card.component.ts
+++ my-angular-albums/src/app/albums/album-card/album-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { Album } from "../album.model";
 
 @Component({
@@ -9,6 +9,11 @@ import { Album } from "../album.model";
 export class AlbumCardComponent implements OnInit {
   @Input()
   album: Album;
+  @Output()
+  albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
+  showAlbum() {
+    this.albumClicked.emit(this.album);
+}
 
   constructor() {}
 
