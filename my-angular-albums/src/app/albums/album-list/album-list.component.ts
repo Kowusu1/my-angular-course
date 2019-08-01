5f7a7d5be02a9b2fafdbd1693def57e1c70d953e
Kevin Owusu-Sekyere <kowusu1@umbc.edu>
2019-08-01 11:02:29 -0400

    chp 8

--- my-angular-albums/src/app/albums/album-list/album-list.component.ts
+++ my-angular-albums/src/app/albums/album-list/album-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 
+import { AlbumService } from "../shared/album.service"
 import { Album } from "../album.model";
-import { ALBUMS } from "../albums.data";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: "app-album-list",
@@ -9,15 +10,19 @@ import { ALBUMS } from "../albums.data";
   styleUrls: ["./album-list.component.css"]
 })
 export class AlbumListComponent implements OnInit {
-  albumsArray: Album[];
+  albumsArray: Observable<Album[]>;
+
+  constructor(private albumService: AlbumService) { }
 
   ngOnInit(): void {
-    this.albumsArray = ALBUMS;
-  }
+    this.getAlbums();
+   }
 
   parentFunctionHandler(album) {
-    alert(
-      "Album " + album.albumName + " was sent from the album card component"
-    );
+    alert('Album ' + album.albumName + ' was sent from the album card component');
+  }
+  
+  getAlbums() {
+    this.albumsArray = this.albumService.getAlbums();
   }
 }
